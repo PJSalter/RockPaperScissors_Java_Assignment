39f09cd59e12590f8715432ffe4240984da45db2
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-04-29 21:05:59 +0100

    Pseudocode comments for logic of my code

--- src/com/game/RockPaperScissors.java
+++ src/com/game/RockPaperScissors.java
@@ -1,5 +1,17 @@
 package com.game;
 
 public class RockPaperScissors {
-//    public
+    public static void main(String[] args){
+        // Create a new Scanner object to read user input.
+        // Create a new Random object to generate computer's choice.
+        // Initialize variables for the number of rounds, user's choice, computer's choice,
+        // number of ties, number of user wins, and number of computer win's.
+
+        // Prompt the user to enter the number of rounds to play.
+        // Read the user's input using the Scanner object.
+
+        // Check if the number of rounds entered by the user is valid.
+        // If the number of rounds is less than 1 or greater than 10, print an error message and exit the program.
+    }
+
 }
